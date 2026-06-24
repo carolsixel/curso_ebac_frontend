1ecc3fa8912ac51ad6a920e586071a4d9dd0f318
Caroline Sixel <caroline.sixels@gmail.com>
2026-06-24 18:15:44 -0300

    TypeScript

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+function dizOla(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(5, 3));
+console.log(dizOla("Caroline"));
+//# sourceMappingURL=exercicio.js.map
---
+++ src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+function dizOla(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicar(5, 3));
+console.log(dizOla("Caroline"));
